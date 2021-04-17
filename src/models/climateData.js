cec2e47efb66613a77200ffea5b67ef091bd9753
Yoshida-Eduardo <edry97@hotmail.com>
2021-04-17 16:24:49 -0300

    Adds csv generation to models

--- src/models/climateData.js
+++ src/models/climateData.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const toCsv = require ("../utils/csv");
 
 const { Schema } = mongoose;
 
@@ -17,10 +18,31 @@ const ClimateDataSchema = new Schema(
     wind: {
       type: String,
     },
+    co2: {
+      type: String
+    }
   },
   {
     timestamps: true,
   }
 );
+
+ClimateDataSchema.methods.toCsv = async function(){
+  const latitude = this.generator.latitude;
+  const longitude = this.generator.longitude;
+
+  const fields = [
+    { label: "Latitude", value: () => latitude },
+    { label: "Longitude", value: () => longitude},
+    { label: "Umidade(RH)", value: "umidity" },
+    { label: "Temperatura(ºC)", value: "temperature", default: null },
+    { label: "Vento(m/s)", value: "wind", default: null },
+    { label: "CO² Gasoso(ppm)", value: "co2", default: null },
+    { label: "Horário da Medição", value: "createdAt", default: null }
+  ]
+
+  const csv = await toCsv.transform(this.climateData, fields)
+  return csv;
+}
 
 module.exports = mongoose.model("ClimateData", ClimateDataSchema);
