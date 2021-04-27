7a8892e5d9f6753bf435f25d55e5169158b07889
guilherme.moreira <guilherme.moreira@coopersystem.com.br>
2021-04-27 11:30:27 -0300

    adding new seeds

---
+++ db/helper.js
@@ -0,0 +1,5 @@
+module.exports = {
+    randomNumber: (min, max) => {
+        return Math.random() * (max - min) + min;
+      }
+}
--- db/seeds.js
+++ db/seeds.js
@@ -2,6 +2,7 @@ const seeder = require('mongoose-seed');
 const uuid = require('uuid');
 const faker = require('faker');
 const id = require('mongoose').Types.ObjectId;
+const helper = require('./helper');
 const gen1 = uuid.v4();
 const gen2 = uuid.v4();
 
@@ -43,12 +44,13 @@ let energy = [];
 for(let j = 0; j < 20; j++){
   climate.push({
     generator: j % 2 == 0 ? gen1 : gen2,
-    temperature: faker.datatype.number().toString(),
-    umidity: faker.datatype.number().toString(),
-    windSpeed: faker.datatype.number().toString(),
+    temperature: helper.randomNumber(-40, 125).toString(),
+    umidity: helper.randomNumber(0, 100).toString(),
+    windSpeed: helper.randomNumber(0, 33).toString(),
     windDirection: faker.address.direction(),
-    co2: faker.datatype.number().toString(),
-    rain: faker.datatype.number().toString(),
+    co2: helper.randomNumber(0, 2000).toString(),
+    rain: helper.randomNumber(0, 20).toString(),
+    createdAt: new Date().setHours(new Date().getHours() + j),
     _id: id()
   })
 }
@@ -57,14 +59,15 @@ for(let j = 0; j < 20; j++){
   energy.push({
     generator: j % 2 == 0 ? gen1 : gen2,
     type: j % 2 == 0 ? "turbine" : "panel",
-    averageInputTension: faker.datatype.number().toString(),
-    averageOutputTension: faker.datatype.number().toString(),
-    averageOutputCurrent: faker.datatype.number().toString(),
-    outputTensionSpike: faker.datatype.number().toString(),
-    outputCurrentSpike: faker.datatype.number().toString(),
+    averageInputTension: helper.randomNumber(0, 50).toString(),
+    averageOutputTension: helper.randomNumber(190, 240).toString(),
+    averageOutputCurrent: helper.randomNumber().toString(),
+    outputTensionSpike: faker.datatype.number(0, 240).toString(),
+    outputCurrentSpike: helper.randomNumber(0, 5).toString(),
     averageBladeFrequency: faker.datatype.number().toString(),
-    averageSupply: faker.datatype.number().toString(),
-    tension: faker.datatype.number().toString(),
+    averageSupply: helper.randomNumber(0, 1000).toString(),
+    tension: helper.randomNumber(0, 50).toString(),
+    createdAt: new Date().setHours(new Date().getHours() + j),
     _id: id()
   })
 }
